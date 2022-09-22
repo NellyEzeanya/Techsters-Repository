6ab6c90d5730291e4c7883026cd142ea618a9785
Nelly <nelianogs@gmail.com>
2022-09-22 12:28:50 +0100

    This are my changes

---
+++ women-techstersjs/script.js
@@ -0,0 +1,22 @@
+// alert("I am done");
+
+// let firstChildAge = 20;
+// var secondChildAge = 15;
+
+// const totalAgeOfChildren = firstChildAge + secondChildAge
+
+// alert(totalAgeOfChildren);
+
+// function anyName() {
+//     const name = "Damilola";
+//     let age = 20;
+//     let anotherAge = 40;
+//     let totalAge = age + anotherAge;
+
+// }
+function add(a,b) {
+    return a + b;
+}
+const value = add(5,6);
+alert(value);
+
